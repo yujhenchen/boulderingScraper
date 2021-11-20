9c7b3a5079067831e4286a3e1bda877dd62d614b
Erik Meade <egmeade@live.com>
2021-11-20 11:58:16 -0800

    Added index file that we can use to render

--- scraper/views.py
+++ scraper/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the scraper index.")
-# Create your views here.
+    return render(request, 'index.html') 
