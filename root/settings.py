69d45d1a7fd688a7123b0b6789e055c8efba360e
Erik Meade <egmeade@live.com>
2021-11-20 11:14:10 -0800

    Update name of module to be root instead of boulderingScraper from rename

--- root/settings.py
+++ root/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boulderingScraper.urls'
+ROOT_URLCONF = 'root.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'boulderingScraper.wsgi.application'
+WSGI_APPLICATION = 'root.wsgi.application'
 
 
 # Database
