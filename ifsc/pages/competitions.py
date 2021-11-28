f304ff3668c5406755cb8690a65b28d9cf71e559
Erik Meade <egmeade@live.com>
2021-11-28 01:32:47 -0800

    Added 2022 comps and athletes

--- ifsc/pages/competitions.py
+++ ifsc/pages/competitions.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Competitions:
@@ -14,13 +14,27 @@ class Competitions:
 
 		driver.get(self.url)
 		wait = WebDriverWait(driver, 5)
-		wait.until(presence_of_element_located((By.CSS_SELECTOR, '#calendar')))
+		wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#calendar')))
 		driver.switch_to.frame('calendar')
 		
-		wait.until(presence_of_element_located((By.CSS_SELECTOR, '.competition')))
+		wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.competition')))
 		events = driver.find_elements(By.CSS_SELECTOR, '.competition .title a')
-
 		eventTitles = []
 		for event in events:
 			eventTitles.append(event.get_attribute('innerHTML'))
-		return eventTitles	
+		
+		yearSelect = driver.find_element(By.ID, 'yearSelect')
+		yearSelect.click()
+		
+		wait.until(EC.presence_of_element_located((By.XPATH, "//select[@id='yearSelect']/option[@value='0']")))
+
+		newestYear = driver.find_element(By.XPATH, "//select[@id='yearSelect']/option[@value='0']")
+		newestYear.click()
+
+		wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '.competition .title a'), '2022'))
+		events = driver.find_elements(By.CSS_SELECTOR, '.competition .title a')
+		for event in events:
+			eventTitles.append(event.get_attribute('innerHTML'))
+
+		
+		return eventTitles
--- scraper/views.py
+++ scraper/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ifsc.pages.competitions import Competitions
+from ifsc.pages.athletes import Athletes
 
 def index(request):
+    print(request.path)
+    path = request.path
+    if path == '/scraper/':
+        return render(request, 'index.html')
 
+def competitions(request):
     competitions = Competitions()
     context = { 'competitions' : competitions.fetchCompetitions()}
-    return render(request, 'index.html', context) 
+    return render(request, 'competitions.html', context)
+
+def athletes(request):
+    athletes = Athletes()
+    context = { 'athletes' : athletes.fetchAthletes()}
+    return render(request, 'athletes.html', context)
+
