6f694cff6f6dec994a8b7f297f15c5dff9fc7f7d
yujhenchen <yujhen.chen93@gmail.com>
2022-01-02 18:19:15 +0800

    fix conda env settings

--- condaEnvSetup.sh
+++ condaEnvSetup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # Update conda to the latest
-conda update conda
+conda update -y conda
 conda --version
 
 # Remove the envronment
 # conda remove - name myenv - all
 # Create new python environment with specific python version
-conda create --name myenv -y python=3.10.0
-
-# Activate environment (Set as working environment)
-# conda activate myenv
-# conda init
-activate myenv # For windows
+conda create --name myenv -y python=3.8
 
 # Install packages using conda on working conda environment
 conda install --name myenv -y -c conda-forge selenium
 conda install --name myenv -y -c conda-forge webdriver-manager
 conda install --name myenv -y -c conda-forge pytest-html
+conda install --name myenv -y -c conda-forge django
+conda install --name myenv -y -c conda-forge backports.zoneinfo
+
+# Activate environment (Set as working environment)
+# conda activate myenv
+# conda init
+conda activate myenv # For windows
 
 # Check installed packages
 # conda list
